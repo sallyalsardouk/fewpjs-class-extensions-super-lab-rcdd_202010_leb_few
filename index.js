e2e19c3f5e581679c7e843f0065d366fe39d2436
sally al sardouk <sally.sardouk98@gmail.com>
2020-12-16 09:03:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ class Deciduous extends Tree{
     this.name=name
   }
   static definition(){
-  return super.definition() +` Deciduous trees shed their leaves annually.;`
+  return super.definition() +` Deciduous trees shed their leaves annually.`
 }
 }
 class Evergreen extends Tree {
