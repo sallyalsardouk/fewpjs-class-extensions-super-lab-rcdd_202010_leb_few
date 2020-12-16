e4b41b73202fbd4dbe501ae0047f59f25921904a
sally al sardouk <sally.sardouk98@gmail.com>
2020-12-16 08:35:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ class Deciduous extends Tree{
     this.name=name
   }
   static definition(){
-   return super(definition)`
+   return super(definition)`Deciduous trees shed their leaves annually.`
 }
 }
